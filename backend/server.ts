b9112a3cc7ba29655cd3edb638b323af2ba9588e
Sam Hudson <sam.hud@icloud.com>
2022-11-26 13:02:32 -0800

    Port changes

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,9 @@ import api from "./routes/api";
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
 const cors = require("cors");
+
+const PORT = process.env.PORT || 3030;
+
 app.use(express.json()); // Enables JSON parsing
 app.use(cors());
 
@@ -12,4 +15,6 @@ console.log("Backend running");
 // Routes go here
 app.use("/", api); // 3. adds the recipe endpoints
 
-app.listen(3001); // 4. runs Express
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
