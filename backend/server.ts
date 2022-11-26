20b737d28e01d9bb177f01a4715c0e82ad210cb2
Sam Hudson <sam.hud@icloud.com>
2022-11-26 13:33:41 -0800

    Backend deploy v2

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Enables JSON parsing
 app.use(cors());
