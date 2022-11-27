707b54fa02573b09787fef7a9ad70bc768fe2049
Sam Hudson <sam.hud@icloud.com>
2022-11-26 17:57:15 -0800

    Fixed CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
 import api from "./routes/api";
 
+const express = require("express"); // Includes express
 const server = express();
+const cors = require("cors");
 
 const port = 3000;
+
+server.use(cors());
+server.use(express.json()); // Enables JSON parsing
 
 server.use("/", api);
 
